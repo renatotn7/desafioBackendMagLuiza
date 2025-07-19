9ac352e61f491e369fbb63fa4324e2a887f2fbff
Renato Tomaz Nati <renatotnati@apoioprodesp.sp.gov.br>
2025-07-19 19:31:44 -0300

    commit inicial

---
+++ src/test/java/com/magazineluiza/favoritos/FavoritosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magazineluiza.favoritos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoritosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
